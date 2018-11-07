8362b519574cdb24a6a45390b1bc8ed931a85232
Mac Dowell Prosper <macdowell@noukod.com>
2018-11-07 19:04:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,6 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
     new_student = new
     new_student.id = row[0]
     new_student.name = row[1]
@@ -17,8 +16,13 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      new_from_db(row)
+    end.first
   end
 
   def save
